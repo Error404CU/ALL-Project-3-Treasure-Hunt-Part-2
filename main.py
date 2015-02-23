fb33c021f7a3a3f7046dcbf8b643b02a808f2df0
iRaduxD <bunciur@uni.coventry.ac.uk>
2015-02-23 16:21:42 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# Importing libraries
+from Tkinter import *
+from RobotClass import *
+import random
+import time
+
+# Window Defining
+window = Tk()
+window.title('Treasure Hunt!')
+canvas = Canvas(window, width = 800, height = 800, bg = 'white')
+canvas.pack(padx = 0, pady = 0)
+
