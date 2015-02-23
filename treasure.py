18cd272b1a6b19c90a7e653203cc2a156a82b2ef
Rahat Haider <haiderr@uni.coventry.ac.uk>
2015-02-23 16:47:16 +0000

    treasure.py

--- treasure.py
+++ treasure.py
@@ -1,4 +1,5 @@
 from placeableObject import *
+from RobotClass import *
 
 class treasure(object):
     def __init__(self, v = 1, s = 0, score = 0):
@@ -10,3 +11,6 @@ class treasure(object):
         self.canvas = canvas
         self.p = self.canvas.create_image(self.x, self.y, image = gif1, anchor = NW)
         self.canvas.update()
+
+    def treasureFound(self, canvas):
+        
