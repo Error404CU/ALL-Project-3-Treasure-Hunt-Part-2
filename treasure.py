e9a6012ac2eb1ad7be78a3a47b1473edbdb3d5cf
Rahat Haider <haiderr@uni.coventry.ac.uk>
2015-02-23 16:24:32 +0000

    treasure class

--- treasure.py
+++ treasure.py
@@ -2,4 +2,11 @@ from placeableObject import *
 
 class treasure(object):
     def __init__(self, v = 1, s = 0, score = 0):
-        
+        self.v = v
+        self.s = s
+        self.score = score
+
+    def drawTreasure(self, canvas):
+        self.canvas = canvas
+        self.p = self.canvas.create_image(self.x, self.y, image = gif1, anchor = NW)
+        self.canvas.update()
