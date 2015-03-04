22cb229cd4066f894ffe0adf7acc80302867abf6
Rahat Haider <haiderr@uni.coventry.ac.uk>
2015-03-04 11:16:45 +0000

    Updated treasure class

--- treasure.py
+++ treasure.py
@@ -1,16 +1,13 @@
-from placeableObject import *
-from RobotClass import *
-
-class treasure(object):
-    def __init__(self, v = 1, s = 0, score = 0):
-        self.v = v
-        self.s = s
-        self.score = score
-
+class Treasure():
+    def __init__(self, x, y, size, colour = "green"):
+        self.x = x
+        self.y = y
+        self.size = size
+        self.colour = colour
     def drawTreasure(self, canvas):
         self.canvas = canvas
-        self.p = self.canvas.create_image(self.x, self.y, image = gif1, anchor = NW)
-        self.canvas.update()
-
-    def treasureFound(self, canvas):
-        
+        self.shape = canvas.create_rectangle(self.x, self.y, self.x + self.size, self.y + self.size, fill = self.colour)
+    def deleteTreasure(self, canvas, shape):
+         self.canvas = canvas
+         self.shape = shape
+         self.canvas.update()
